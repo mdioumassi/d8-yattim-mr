974602cb17968e4e7c6e921733e59d0f067b3cc3
mdioumassi <mdioumassi@yahoo.fr>
2020-12-02 20:31:49 +0100

    gitignore

---
+++ web/modules/custom/donate/src/Entity/DonateInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Drupal\donate\Entity;
+
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+interface  DonateInterface extends ContentEntityInterface
+{
+
+}
---
+++ web/themes/custom/yattim/js/script.js
@@ -0,0 +1,6 @@
+(function($, D){
+  $(document).ready(function(){
+   // $("#navbar").addClass("w3-green");
+    //$("#navbar-collapse nav.active").addClass("w3-light-green");
+  });
+})(jQuery, Drupal);
